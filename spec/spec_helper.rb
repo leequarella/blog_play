7be7e1ee05453da96ada91deb3518ef384635d68
Lee Quarella <lee@lucidfrog.com>
2012-12-19 17:06:09 -0500

    added db.clean into prefork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ Spork.prefork do
   #require 'capybara/rspec'
 
   DatabaseCleaner.strategy = :truncation
+  DatabaseCleaner.clean
   
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
