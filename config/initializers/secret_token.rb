abbda94e09d819bdc5d305c20efe711ae90ab357
Caleb Everett <everett.caleb@gmail.com>
2012-12-19 10:45:48 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogPlay::Application.config.secret_token = '5100b74a75937ca2992cb06dc081e9867aec4120b2ded7eab4e3fff58d4a034a525df133ca24c29da866f165812f421280f803e7369bf6caac322ca2d90163e1'
